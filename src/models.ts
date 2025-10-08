9e0c98c09b84b94840a06d05f9e9ef17fb29790f
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2025-10-08 10:42:49 -0700

    Add support to use fingerprint filtering

--- src/models.ts
+++ src/models.ts
@@ -1,17 +1,21 @@
 export interface AdUnitsFetchModel {
-  /**
-   * The search query string. Must be at least 2 characters long.
-   */
-  query: string;
+    /**
+     * The search query string. Must be at least 2 characters long.
+     */
+    query: string;
+    /**
+     * Should 'fingerprint' based filtering be used or not.
+     */
+    use_fingerprint_filtering: boolean;
 }
 
 export interface AdUnitsIntegrateModel {
-  /**
-   * A list of ad unit IDs to integrate.
-   */
-  ad_unit_ids: string[];
-  /**
-   * The base content for integration. Must be at least 10 characters long.
-   */
-  base_content: string;
+    /**
+     * A list of ad unit IDs to integrate.
+     */
+    ad_unit_ids: string[];
+    /**
+     * The base content for integration. Must be at least 10 characters long.
+     */
+    base_content: string;
 }
